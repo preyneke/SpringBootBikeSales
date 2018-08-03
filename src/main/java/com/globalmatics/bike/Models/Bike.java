67d93b90ff3c7efea1306753c2fcd263703f6259
pieter <preyneke@gmail.com>
2018-08-03 12:58:45 +0200

    Entity class set up

--- src/main/java/com/globalmatics/bike/Models/Bike.java
+++ src/main/java/com/globalmatics/bike/Models/Bike.java
@@ -1,9 +1,19 @@
 package com.globalmatics.bike.Models;
 
+import org.springframework.boot.autoconfigure.web.ResourceProperties;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.util.Date;
 
+@Entity
 public class Bike {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String name;
     private String email;
     private String phone;
@@ -12,6 +22,15 @@ public class Bike {
     private BigDecimal purchasePrice;
     private Date purchaseDate;
     private boolean contact;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
